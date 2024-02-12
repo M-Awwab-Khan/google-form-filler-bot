64c64c4d7aef9180634745c3f048ca9855c6f2e5
Awwab <mawwabkhank2006@gmail.com>
2024-02-12 11:04:18 +0500

    removed unnecessary imports

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import ElementClickInterceptedException
-import time
 from constants import *
 
 response = requests.get(ZILLOW_LINK)
