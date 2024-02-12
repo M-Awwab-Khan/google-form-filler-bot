b0f0e39df2d76d8f2a97f079333d29efcb8b96e1
Awwab <mawwabkhank2006@gmail.com>
2024-02-12 09:55:07 +0500

    setup constants

---
+++ constants.py
@@ -0,0 +1,2 @@
+ZILLOW_LINK = 'https://appbrewery.github.io/Zillow-Clone/'
+GOOGLE_FORM = 'https://docs.google.com/forms/d/e/1FAIpQLScKdk-k8KsdcQ45U7PiNyAC8hDjvrsVi13UtjAWCNfyI9_UcQ/viewform?usp=sf_link'
